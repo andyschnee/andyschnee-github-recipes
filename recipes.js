1ca486fad45de9ce43775e4f464794a34e8081db
andyschnee <hall.andy@gmail.com>
2023-12-12 09:22:24 +0000

    0922

--- recipes.js
+++ recipes.js
@@ -58,7 +58,7 @@ function searchJson(obj, searchTerm) {
 }
 
 function urlEvent() {
-    console.debug('url');
+    $('body').prepend('0922');
 
     // ${$('#sectionRecipe').text()}
     let urlEvent = `https://www.google.com/url?q=https://calendar.google.com/calendar/u/0/r/eventedit?text=${$('#pName').text()}&dates=20231213T190000Z/20231213T210000Z&details=test&src=oo9ibfskdn845lhfns53gntbvo%2540group.calendar.google.com`;
